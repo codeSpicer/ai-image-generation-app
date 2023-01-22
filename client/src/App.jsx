307df73b936cadfa96a5e94da27ac9ecbf42aaa6
Akshat kumar <AKSHAT.LKO.2001@GMAIL.COM>
2023-01-23 00:35:05 +0530

    initial setup
    setting up react project with tailwindcss and installing dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className=""></header>
+    </BrowserRouter>
+  );
+};
+
+export default App;
